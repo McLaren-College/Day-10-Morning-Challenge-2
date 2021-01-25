9334a59ad935d1754d9198043c5f851d1f5b1962
McLaren College <53505683+McLarenCollege@users.noreply.github.com>
2021-01-25 08:23:31 +0530

    Update main.test.js

--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-const { isModest, sortContacts } = require('./main');
+const { isModest } = require('./main');
 
 test('Exercise 1 - Test cases', () => {
     expect(isModest(2036)).toBe(true);
@@ -7,61 +7,4 @@ test('Exercise 1 - Test cases', () => {
     expect(isModest(2039)).toBe(false);
     expect(isModest(23)).toBe(true);
     expect(isModest(11)).toBe(false);
-});
-
-test('Exercise 2 test cases', () => {
-    expect(JSON.stringify(sortContacts([
-        "John Locke",
-        "Thomas Aquinas",
-        "David Hume",
-        "Rene Descartes"
-    ], "ASC"))).toBe(JSON.stringify([
-        "Thomas Aquinas",
-        "Rene Descartes",
-        "David Hume",
-        "John Locke"]));
-
-    expect(JSON.stringify(sortContacts([
-        "John Locke",
-        "Thomas Aquinas",
-        "David Hume",
-        "Rene Descartes"
-    ], "DESC"))).toBe(JSON.stringify([
-        "John Locke",
-        "David Hume",
-        "Rene Descartes",
-        "Thomas Aquinas"]));
-
-    expect(JSON.stringify(sortContacts([
-        "A B",
-        "C D",
-        "E F",
-        "G H"
-    ], "ASC"))).toBe(JSON.stringify([
-        "A B",
-        "C D",
-        "E F",
-        "G H"]));
-
-    expect(JSON.stringify(sortContacts([
-        "A B",
-        "C D",
-        "E F",
-        "G H"
-    ], "DESC"))).toBe(JSON.stringify([
-        "G H",
-        "E F",
-        "C D",
-        "A B"]));
-
-    expect(JSON.stringify(sortContacts([
-        "A B"
-    ], "DESC"))).toBe(JSON.stringify([
-        "A B"]));
-
-    expect(JSON.stringify(sortContacts([
-        "A B"
-    ], "ASC"))).toBe(JSON.stringify([
-        "A B"]));
-
 });
